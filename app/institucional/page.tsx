59b9e5b75fcd60a333104d453258cb06cc15b5ec
Leandro Filipe <you@example.com>
2026-04-13 20:27:39 -0300

    r

--- app/institucional/page.tsx
+++ app/institucional/page.tsx
@@ -145,8 +145,8 @@ export default function PaginaInstitucional() {
             <div className="flex flex-wrap gap-8 opacity-60">
                 <p className="text-[9px] font-black uppercase tracking-widest">Conselho Fiscal: WLADINEY CERBINO FERREIRA</p>
                 <p className="text-[9px] font-black uppercase tracking-widest">Conselho Fiscal: RODRIGO MOREIRA ABRANTES</p>
-                <p className="text-[9px] font-black uppercase tracking-widest">Conselho Fiscal: WLADINEY CERBINO FERREIRA</p>
-                <p className="text-[9px] font-black uppercase tracking-widest">Suplente: ELIAS CASSIA CASSIANO DA SILVA JUNIOR.</p>
+                <p className="text-[9px] font-black uppercase tracking-widest">Conselho Fiscal: ELIAS CASSIA CASSIANO DA SILVA JUNIOR.</p>
+                <p className="text-[9px] font-black uppercase tracking-widest">Suplente: ARCELI CHAVES.</p>
                 <p className="text-[9px] font-black uppercase tracking-widest">Suplente: RAPHAEL VASCONCELOS BANDEIRA GONÇALVES.</p>
                 <p className="text-[9px] font-black uppercase tracking-widest">Suplente: THOMPSON AZEVEDO MOREIRA.</p>
 
